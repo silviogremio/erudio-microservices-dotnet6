4bb3573dfa9b4004439d4205239b0d54b46ea196
Silvio <silviogremio@gmail.com>
2022-03-25 14:19:51 -0300

    Projeto com VO implementado

---
+++ S08_firstMicrosservice_VO/GeekShopping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext() {}
+
+        public MySQLContext( DbContextOptions<MySQLContext> options) : base(options){}
+
+        public DbSet<Product> Products { get; set; }
+
+        //Para migrations:
+        //botão diretiro em cima do nome do projeto/open in terminal
+        //1. Executar(Instalação do ef): dotnet tool install --global dotnet-ef
+        //2. Executar:dotnet ef migrations add AddProductDataTableOnDB
+        //2.1 Executar(Se quiser Remover): dotnet ef migrations remove
+        //3. Para compilar no banco basta: dotnet ef database update
+
+
+    }
+}
---
+++ S08_firstMicrosservice_VO/GeekShopping/GeekShopping.ProductAPI/Model/Product.cs
@@ -0,0 +1,37 @@
+﻿using GeekShopping.ProductAPI.Model.Base;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.ProductAPI.Model
+{
+    //Serve para fazer o binding entre o banco e a aplicação.
+    //se a tebela chamasse produto, o class poderia ser Product em table = produto.
+    //nesse caso a tabela se chamará Product também.
+    [Table("product")]
+    public class Product : BaseEntity
+    {
+        [Column("name")]
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+
+        [Column("price")]
+        [Required]
+        [Range(1,10000)]
+        public decimal Price { get; set; }
+
+        [Column("description")]
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Column("category_name")]
+        [StringLength(50)]
+        public  string CategoryName { get; set; }
+
+        [Column("image_url")]
+        [StringLength(300)]
+        public string ImageUrl { get; set; }
+
+    }
+}
