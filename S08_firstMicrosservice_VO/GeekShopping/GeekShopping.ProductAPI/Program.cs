6781271ff81355933247b9f258b38cefcb0eeb95
Silvio <silviogremio@gmail.com>
2022-03-25 17:46:03 -0300

    Subindo Imagens e VO concluído

--- S08_firstMicrosservice_VO/GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ S08_firstMicrosservice_VO/GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,4 +1,7 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
+using GeekShopping.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,9 +15,23 @@ builder.Services.AddSwaggerGen();
 
 
 //****ini: Não tem classe Startup
-var connection = builder.Configuration["MySqlConnection:MysqlConnectionString"];
 
+//ini: Add conexão com o BD
+var connection = builder.Configuration["MySqlConnection:MysqlConnectionString"];
 builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 23))));
+//fim
+
+//ini: Add Automapper para fazer VO
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+//fim
+
+//ini: adicionando Interface e classe reposity de Product
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+//fim
 
 //****fim
 
