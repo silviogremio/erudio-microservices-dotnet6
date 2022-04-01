635652727806e50d7ee45e357b6cec9831b43498
Silvio <silviogremio@gmail.com>
2022-04-01 13:25:32 -0300

    Finalizada implementação de métodos API

---
+++ S08_firstMicrosservice_VO/GeekShopping/GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,65 @@
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            //tratando execeção de vir nulo, se Nulo lança a exceção
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
+        {
+            var products = await _repository.FindAll();
+            return Ok(products);
+        }
+    
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductVO>> FindById(long id)
+        {
+            var product = await _repository.FindById(id);
+
+            if (product == null) return NotFound();
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductVO>> Create(ProductVO productVO)
+        {
+            if (productVO == null) return BadRequest();
+            productVO = await _repository.Create(productVO);
+            return Ok(productVO);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ProductVO>> Update(ProductVO productVO)
+        {
+            if (productVO == null) return BadRequest();
+            productVO = await _repository.Update(productVO);
+            return Ok(productVO);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(long id)
+        {
+            var status = await _repository.Delete(id);
+
+            //Se o retorno for falso
+            if (!status) return BadRequest();
+
+            return Ok(status);
+        }
+
+    }
+}
