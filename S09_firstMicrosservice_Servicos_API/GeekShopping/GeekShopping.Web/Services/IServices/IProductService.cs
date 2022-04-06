71f4524ec6d8295ca7f8cad74e89af1352f23a42
Silvio <silviogremio@gmail.com>
2022-04-06 16:32:04 -0300

    API finalizada e start loja que consome API

---
+++ S09_firstMicrosservice_Servicos_API/GeekShopping/GeekShopping.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using GeekShopping.Web.Models;
+
+namespace GeekShopping.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        public Task<IEnumerable<ProductModel>> FindAllProducts();
+        public Task<ProductModel> FindProductById(long id);
+        public Task<ProductModel> CreateProduct(ProductModel model);
+        public Task<ProductModel> UpdateProduct(ProductModel model);
+        public Task<bool> DeleteProductById(long id);
+
+    }
+}
---
+++ S10_firstMicrosservice_Servicos_Loja/GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -0,0 +1,59 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
+using GeekShopping.ProductAPI.Model.Context;
+using GeekShopping.ProductAPI.Repository;
+using GeekShopping.Web.Services;
+using GeekShopping.Web.Services.IServices;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+//****ini: Não tem classe Startup
+
+//ini: Add conexão com o BD
+var connection = builder.Configuration["MySqlConnection:MysqlConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 23))));
+//fim
+
+//ini: Add Automapper para fazer VO
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+//fim
+
+//ini: adicionando Interface e classe reposity de Product
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+//injetando ProductService com o HttpClient para consumir os Jsons
+//????????????
+builder.Services.AddHttpClient<IProductService, ProductService>(c =>
+    c.BaseAddress = new Uri(builder.Configuration["ServicesUrls:ProductAPI"])
+    );
+
+//fim
+
+//****fim
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
